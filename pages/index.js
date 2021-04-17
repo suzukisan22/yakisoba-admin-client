f4b4213684fa7a3bdb1623256db5c7ba1b812aea
suzuki.r <getty2017@gmail.com>
2021-04-17 21:30:27 +0900

    ログイン処理を実装

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useEffect, useState} from 'react'
 import axios from 'axios'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const [users, setUsers] = useState([])
--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,48 @@
+import { useState } from 'react';
+import styles from '../styles/Home.module.css';
+import { Button, TextField } from '@material-ui/core';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
 export default function Login() {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter();
+
+  const submitLogin = () => {
+     axios.post('http://localhost:3000/v1/admin/login', {
+      username,
+      password
+    }).then(async (response) => {
+      await localStorage.setItem("r_to_a_admin_key", response.data.auth_token)
+      router.push('http://localhost:8080/')
+    }).catch((response) => {
+    })
+  }
+
   return (
-    <div>
+    <div className={styles.container}>
       <h1>ログイン</h1>
+      <TextField 
+        label="username"
+        name="username"
+        value={username}
+        onChange={ (e) => { 
+          setUsername(e.target.value)
+        }}
+      />
+      <TextField
+        label="password"
+        name="password"
+        type="password"
+        onChange={
+          (e) => { setPassword(e.target.value) }
+        }
+      />
+      <Button variant="contained" color="primary" onClick={submitLogin} >
+        ログイン
+      </Button>
     </div>
   )
 }
