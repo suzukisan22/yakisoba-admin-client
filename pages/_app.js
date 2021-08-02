65dd6e594463aa7557fd3ebe9750989efaae391d
suzuki.r <getty2017@gmail.com>
2021-08-02 22:36:19 +0900

    noindex設定

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import React from 'react'
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <Head>
+        <title>R To A管理画面</title>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 
 export default MyApp
