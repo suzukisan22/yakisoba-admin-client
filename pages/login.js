a4169af1a89289ae027d527838e14ddeb169b0bb
suzuki.r <getty2017@gmail.com>
2021-11-07 12:58:47 +0900

    class→classNameに変更

--- pages/login.js
+++ pages/login.js
@@ -24,7 +24,7 @@ export default function Login() {
   return (
     <div className={styles.loginContainer}>
       <h1>ログイン</h1>
-      <div class={styles.formWrapper}>
+      <div className={styles.formWrapper}>
         <TextField 
           label="username"
           name="username"
