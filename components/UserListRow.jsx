6f057df310f55cb5976d3006d3fde4d03e6ce770
suzuki.r <getty2017@gmail.com>
2021-11-19 16:18:51 +0900

    rowを別componentに分ける

---
+++ components/UserListRow.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import axios from 'axios'
+import styles from '../styles/Home.module.scss'
+import Link from 'next/link';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import {Popover} from '@material-ui/core';
+
+export function UserListRow({propsUser, propsAttendance, isLastRow, propsTransferFeeManager}){
+  const router = useRouter();
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const [user, setUser] = useState(propsUser)
+  const [attendance, setAttendance] = useState(propsAttendance)
+  const [transferFeeManager, setTransferFeeManager] = useState(propsTransferFeeManager)
+
+  const handleMoreVertClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  const onClickAttendance = () => {
+    const authToken = localStorage.getItem('r_to_a_admin_key')
+    axios.post(`${process.env.API_SERVER_ENDPOINT}/v1/admin/users/attendances/update`, {
+      user: {
+        id: user.id,
+        attendance: {
+          is_attende_on_that_day: !attendance.is_attende_on_that_day
+        },
+        transfer_fee_manager: {
+          is_guest_accepted: transferFeeManager.is_guest_accepted
+        },
+      }
+    },
+      {
+      headers: {
+        Authorization: authToken
+      }
+    }).then((response) => {
+      setUser(response.data)
+      setAttendance({...response.data.attendance})
+      setTransferFeeManager({...response.data.transfer_fee_manager})
+      setAnchorEl(null);
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
+
+  const onClickFeeAccepted = () => {
+    const authToken = localStorage.getItem('r_to_a_admin_key')
+    axios.post(`${process.env.API_SERVER_ENDPOINT}/v1/admin/users/attendances/update`, {
+      user: {
+        id: user.id,
+        attendance: {
+          is_attende_on_that_day: attendance.is_attende_on_that_day
+        },
+        transfer_fee_manager: {
+          is_guest_accepted: !transferFeeManager.is_guest_accepted
+        },
+      }
+    },
+      {
+      headers: {
+        Authorization: authToken
+      }
+    }).then((response) => {
+      setUser(response.data)
+      setAttendance({...response.data.attendance})
+      setTransferFeeManager({...response.data.transfer_fee_manager})
+      setAnchorEl(null);
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
+
+  return (
+    <tr key={user.id} className={isLastRow ? '' : styles.borderBottomRequired}>
+      <td>
+        <Link href={`/users/${user.id}`}>
+          <a style={{display: 'block', width: '100%'}}>
+            <span style={{display: 'block', fontSize: '12px', textAlign: "center"}}>{user.last_name_kana}&nbsp;{user.first_name_kana}</span>
+            {user.last_name}&nbsp;{user.first_name}
+          </a>
+        </Link>
+      </td>
+      <td>
+        <Link href={`/users/${user.id}`}>
+          <a style={{display: 'block', width: '100%'}}>
+            {!!user.attendance ? (user.attendance.will_attende ? "出席予定" : "欠席予定") : "未回答"}
+          </a>
+        </Link>
+      </td>
+      <td>
+        <Link href={`/users/${user.id}`}>
+          <a style={{display: 'block', width: '100%'}}>
+            {!!user.attendance ? (user.attendance.is_attende_on_that_day ? "受付済み" : "受付が未完了") : "未回答"}
+          </a>
+        </Link>
+      </td>
+      <td>
+        <Link href={`/users/${user.id}`}>
+          <a style={{display: 'block', width: '100%'}}>
+            {!!transferFeeManager ? (transferFeeManager.cost == 0 ? "提供の必要なし" : (user.transfer_fee_manager.is_guest_accepted ? "提供済み" : "未提供")) : "未回答"}
+          </a>
+        </Link>
+      </td>
+      <td key={user.id}>
+        {(!!attendance || transferFeeManager.cost != 0) &&
+          (
+          <span>
+            <MoreVertIcon onClick={handleMoreVertClick} />
+            <Popover 
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+            >
+              <ul className={styles.popoverList}>
+                {attendance.is_attende_on_that_day ? <li onClick={onClickAttendance}>受付が未完了に戻す</li> : <li onClick={() => onClickAttendance(user)}>受付済みにする</li>}
+                {
+                  (transferFeeManager.cost != 0) && (transferFeeManager.is_guest_accepted ?
+                  <li onClick={onClickFeeAccepted}>お車代を未提供に戻す</li> :
+                  <li onClick={onClickFeeAccepted}>お車代を提供済みにする</li>)
+                }
+            </ul>
+          </Popover>
+          </span>
+          )
+        }
+      </td>
+    </tr>
+  )
+}
---
+++ components/pages/UserList.jsx
@@ -0,0 +1,87 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.scss'
+import React, { useEffect, useState, SyntheticEvent } from 'react'
+import axios from 'axios'
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { UserListRow } from "../UserListRow";
+
+export default function UserList() {
+  const [users, setUsers] = useState([])
+  const [isGroomTabSelected, setIsGroomTabSelected] = useState(true)
+  const router = useRouter();
+
+  useEffect(() => {
+    const authToken = localStorage.getItem('r_to_a_admin_key')
+    const f = async () => {
+      await axios.get(`${process.env.API_SERVER_ENDPOINT}/v1/admin/users?is_groom_side=true`, {
+        headers: {
+          Authorization: authToken
+        }
+      }).then((response) => {
+        setUsers(response.data)
+      }).catch(() => {
+        router.push('/login')
+      })
+    }
+    f()
+  }, [])
+
+  const setUserByInvitationSide = ({isGroomSide})  => {
+    const authToken = localStorage.getItem('r_to_a_admin_key')
+
+    axios.get(`${process.env.API_SERVER_ENDPOINT}/v1/admin/users?is_groom_side=${isGroomSide}`, {
+      headers: {
+        Authorization: authToken
+      }
+    }).then((response) => {
+      setIsGroomTabSelected(isGroomSide)
+      setUsers(response.data)
+    }).catch((e) => {
+    })
+  }
+
+  return (
+    <div>
+      <header className={styles.header}>
+        <span>鈴木家・大河内家結婚式 管理画面</span>
+      </header>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>出席者一覧</h1>
+        <div className={styles.listFilterTab}>
+          <div className={isGroomTabSelected ? styles.groomTabSelected : styles.groomTab} onClick={() => setUserByInvitationSide({isGroomSide: true})}>
+            <span className={isGroomTabSelected ? styles.tabSelectedText : styles.tabText}>新郎側</span>
+          </div>
+          <div className={isGroomTabSelected ? styles.brideTab : styles.brideTabSelected} onClick={() => setUserByInvitationSide({isGroomSide: false})}>
+            <span className={isGroomTabSelected ? styles.tabText : styles.tabSelectedText}>新婦側</span>
+          </div>
+        </div>
+        <table className={styles.attendanceListTable}>
+          <thead>
+            <tr>
+              <th>名前</th>
+              <th>出席予定か</th>
+              <th>受付済み</th>
+              <th>お車代の提供有無</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => {
+              return (
+                <UserListRow
+                  propsUser={user}
+                  key={user.id}
+                  propsAttendance={user.attendance}
+                  isLastRow={index + 1 == users.length}
+                  propsTransferFeeManager={user.transfer_fee_manager}
+                />
+              )
+            })}
+          </tbody>
+        </table>
+      </main>
+    </div>
+  )
+}
